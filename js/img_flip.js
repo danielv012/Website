c3e004d8664a82bde7cbc71c8003023cd9f213d4
Daniel Vayman <daniel@vayman.co>
2023-07-20 09:08:21 -0400

    Added flip animation

--- js/img_flip.js
+++ js/img_flip.js
@@ -1,1 +1,11 @@
-const imageContainer = document.querySelector('.image-container');
+const flipImage = document.getElementById('flipImage2');
+let isFlipped = false;
+
+function removeFlipClass(){
+    flipImage.classList.remove('flip-animation');
+}
+
+flipImage.addEventListener('click', function(){
+    flipImage.classList.add('flip-animation');
+    setTimeout(removeFlipClass, 1000);
+});
