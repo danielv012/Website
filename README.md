c14ce7446ed0985105cefc8c1d741d4673fb3a23
Daniel Vayman <daniel@vayman.co>
2023-07-24 22:53:31 -0500

    Added icons and made progress on about me

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 Self-explanatory. This repository holds the work for my personal website. I'll use it to not only expand my skills for front-end development, but to showcase the work that I do elsewhere.
 
 # TODO:
+- [ ] Fix research section bouncing up and down because of tabs
 - [ ] Add icons next to main headshot
 - [ ] Add icons in contact me section
 - [ ] Fill up about section details
--- js/tab_script.js
+++ js/tab_script.js
@@ -1,7 +1,11 @@
 var tabLinks = document.getElementsByClassName("tab-links");
 var tabContents = document.getElementsByClassName("tab-contents");
+var iconsTab = document.querySelector('.icons');
 
 function opentab(tabname){
+
+    showIcons(tabname)
+    
     for(tablink of tabLinks){
     tablink.classList.remove("active-link");
     }
@@ -12,4 +16,14 @@ function opentab(tabname){
 
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
+}
+
+function showIcons(tabname)
+{
+    if(tabname == 'experience') {
+        iconsTab.style.display = 'flex';
+    }
+    else {
+        iconsTab.style.display = 'none';
+    }
 }
