eb92dc12b52265c06e2417e6bfbf048daf9f6559
Daniel Vayman <daniel@vayman.co>
2023-07-31 16:53:01 -0400

    gif to webm for faster load time

--- js/video_player.js
+++ js/video_player.js
@@ -1,5 +1,22 @@
 // Get the video element
-const video = document.getElementById('project2-video');
+const video = document.getElementById('project1-video');
+
+// Get the second project element
+const project = document.getElementById('slide1');
+
+// Add event listeners for mouseover and mouseout
+project.addEventListener('mouseover', () => {
+  // Play the video when mouse is over the project element
+  video.play();
+});
+
+project.addEventListener('mouseout', () => {
+  // Pause the video when mouse is out of the project element
+  video.pause();
+});
+
+// Get the video element
+const video2 = document.getElementById('project2-video');
 
 // Get the second project element
 const project2 = document.getElementById('slide2');
@@ -7,10 +24,10 @@ const project2 = document.getElementById('slide2');
 // Add event listeners for mouseover and mouseout
 project2.addEventListener('mouseover', () => {
   // Play the video when mouse is over the project element
-  video.play();
+  video2.play();
 });
 
 project2.addEventListener('mouseout', () => {
   // Pause the video when mouse is out of the project element
-  video.pause();
+  video2.pause();
 });
