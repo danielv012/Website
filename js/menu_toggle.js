ab85813085308ca3a383576676f7eef1b72b76c1
Daniel Vayman <daniel@vayman.co>
2023-08-01 14:28:16 -0400

    Done.

--- js/menu_toggle.js
+++ js/menu_toggle.js
@@ -1,9 +1,9 @@
 var sidemenu = document.getElementById("sidemenu");
 
 function openMenu() {
-    sidemenu.style.right = "1rem";
+    sidemenu.style.right = "0rem";
 }
 
 function closeMenu() {
-    sidemenu.style.right = "1rem";
+    sidemenu.style.right = "-12rem";
 }
